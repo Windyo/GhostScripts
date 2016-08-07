fcce5c2d0a3bac1419062e2c45387c64b4ce88c8
Windyo <w_i_n_d_y_o@hotmail.com>
2016-08-07 22:28:49 +0200

    Added Manual Mode
    Allows manual, pre-authentified requests when using a template isn't the best option.

--- APIrequest.sh
+++ APIrequest.sh
@@ -20,6 +20,8 @@ RequestType=""
 Endpoint=""
 AdditionalInfo=""
 AutomaticMode="0"
+ManualMode="0"
+ManualRequest=""
 
 function sanitycheck
 {
@@ -64,19 +66,36 @@ function apirequest
     ClientSecret=`curl $BlogAdress/ghost/signin/ | grep -o -P '(?<=env-clientSecret" content=").*(?=" data-type="string)'`
     BearerToken=`curl --data grant_type=password --data username=$GhostLogin --data password=$GhostPassword --data client_id=ghost-admin --data client_secret=$ClientSecret $BlogAdress/ghost/api/v0.1/authentication/token | grep -o -P '(?<="access_token":").*(?=","refresh_token")'`
 
-  #Make the request
-    declare -a RequestResponse=`curl --trace-ascii trace.txt -X "$RequestType" --header "Authorization: Bearer $BearerToken" "$BlogAdress"/ghost/api/v0.1/"$Endpoint"/?"$AdditionalInfo" | jq '.$Endpoint[]'`
-    for ThisPost in $RequestResponse
-      do
-      echo $ThisPost
-      done
-    echo "Response Printed !"
+
+  if [ "$ManualMode" = "1" ]; then
+    echo "BearerToken : $BearerToken"
+  fi
+
+
+  if [ "$ManualMode" = "0" ]; then
+    #Make the request
+      declare -a RequestResponse=`curl --trace-ascii trace.txt -X "$RequestType" --header "Authorization: Bearer $BearerToken" "$BlogAdress"/ghost/api/v0.1/"$Endpoint"/?"$AdditionalInfo" | jq '.$Endpoint[]'`
+      for ThisPost in $RequestResponse
+        do
+        echo $ThisPost
+        done
+      echo "Response Printed !"
+  else
+     echo "Type in your request here. The script automatically preprends the authorisation headers and logs a trace in trace.txt."
+      read ManualRequest
+      declare -a RequestResponse=`curl trace-ascii trace.txt --header "Authorization: Bearer $BearerToken" "$ManualRequest"`
+      for ThisPost in $RequestResponse
+        do
+        echo $ThisPost
+        done
+      echo "Response Printed !"
+  fi
 
 
 }
 function usage
 {
-    echo "usage: APIrequest.sh [-a (http://blog.mysite.eu ghostlogin ghostpassword RequestType Endpoint AdditionalInfo)] | [-h]]"
+    echo "usage: APIrequest.sh [-a (http://blog.mysite.eu ghostlogin ghostpassword RequestType Endpoint AdditionalInfo)] | [-m (http://blog.mysite.eu ghostlogin ghostpassword cURLrequest)] | [-h]]"
 }
 
 
@@ -91,6 +110,15 @@ while [ "$1" != "" ]; do
                                 RequestType=$4
                                 Endpoint=$5
                                 AdditionalInfo=$6
+                                apirequest
+                                ;;
+        -m | --manualmode )      shift
+                                AutomaticMode=1
+                                ManualMode=1
+                                BlogAdress=$1
+                                GhostLogin=$2
+                                GhostPassword=$3
+                                ManualRequest=$4
                                 apirequest
                                 ;;
         -h | --help )           usage
